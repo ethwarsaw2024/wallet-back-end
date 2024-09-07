c6e531b7ca4557b3766dc5a1dbb3479f35f43956
Maciej Lotkowski <maciej.lotkowski@gmail.com>
2024-09-07 12:48:52 +0200

    Add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
